0df2e3cc49aeec733e68a62f5a5582d273edb0fd
notcodingsound <60252265+notcodingsound@users.noreply.github.com>
2021-04-13 22:24:10 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,35 @@
 
-var synth;
-
-function setup() {
-  noCanvas();
-  synth = new p5.PolySynth();
-  var midiNoteNumber = 70;
-  var freq = midiToFreq(midiNoteNumber); // Convert MIDI note to frequency
-  // Play note number 70 with velocity 1, starting now, for a duration of 1s
-  synth.play(freq, 1.0, 0, 1);
+var sound;
+ 
+function preload()
+{
+  // initialize sound
+  sound = loadSound('vocal.mp3');
+}
+ 
+ 
+function setup() 
+{
+  createCanvas(200, 200);
+  
+  // display instructions
+  textAlign(CENTER);
+  fill(100);
+  noStroke();
+  text("Click to play sound", width/2, height/2);
+}
+ 
+function draw()
+{
+  
+}
+ 
+function mousePressed() 
+{
+  // trigger sound
+  sound.play();
+  
+  // change background color
+  background(random(255), random(255), random(255));
+  
 }
